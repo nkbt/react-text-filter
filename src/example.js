428deaade953a0de6f7bf54dc63db6053557403e
Nik Butenko <nik@butenko.me>
2015-06-12 18:56:08 +1000

    Update example page with runtime component customizations #2

--- src/example.js
+++ src/example.js
@@ -39,22 +39,29 @@ const fruits = [
   'green apple'
 ];
 
+
 const fruitFilter = filter => fruit => fruit.toLowerCase().indexOf(filter.toLowerCase()) !== -1;
+
 
 const App = React.createClass({
   getInitialState() {
-    return {filter: ''};
+    return {
+      filter: '',
+      minLength: 2,
+      debounceTimeout: 300
+    };
   },
 
 
-  onFilter(filter) {
-    this.setState({filter});
+  onChangeDebounceTimeout({target: {value}}) {
+    this.setState({debounceTimeout: parseInt(value, 10)});
   },
 
 
-  fruitFilter(filter) {
-    return rule => rule.name.toLowerCase().indexOf(filter.toLowerCase()) !== -1;
+  onChangeMaxLength({target: {value}}) {
+    this.setState({minLength: parseInt(value, 10)});
   },
+
 
   render() {
     const filteredFruits = this.state.filter ?
@@ -63,8 +70,24 @@ const App = React.createClass({
 
     return (
       <div>
-        <TextFilter onFilter={this.onFilter} />
-        <List items={filteredFruits} />
+        <div>
+          <h2>Customize</h2>
+          minLength:&nbsp;
+          <input type="number" step={1} min={0} max={10}
+            value={this.state.minLength} onChange={this.onChangeMaxLength} />&nbsp;
+          debounceTimeout:&nbsp;
+          <input type="number" step={100} min={0} max={1000}
+            value={this.state.debounceTimeout} onChange={this.onChangeDebounceTimeout} />
+        </div>
+
+        <div>
+          <h2>Test</h2>
+          <TextFilter
+            minLength={this.state.minLength}
+            debounceTimeout={this.state.debounceTimeout}
+            onFilter={filter => this.setState({filter})} />
+          <List items={filteredFruits} />
+        </div>
       </div>
     );
   }
